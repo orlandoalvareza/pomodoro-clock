03bfe906d49c2a98c56cca18a101e6b7576c2f6b
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2024-01-10 21:13:19 -0500

    restructured app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUpLong,
+  faDownLong,
+  faArrowRotateLeft,
+  faPlay,
+  faPause,
+} from '@fortawesome/free-solid-svg-icons';
 
-import './App.css';
+import './style.css';
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
@@ -8,20 +16,6 @@ function App() {
   const [title, setTitle] = useState('Session');
   const [totalTime, setTotalTime] = useState(1500);
   const [isPlay, setIsPlay] = useState(false);
-
-  useEffect(() => {
-    let timeOut = null;
-
-    if (totalTime >= 0 && isPlay) {
-      timeOut = setTimeout(() => {
-        setTotalTime(totalTime - 1);
-      }, 1000);
-    }
-
-    changeTimer();
-
-    return () => clearTimeout(timeOut);
-  }, [isPlay, totalTime]);
 
   const incrementBreak = () => {
     breakLength < 60 && setBreakLength(breakLength + 1);
@@ -72,6 +66,31 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    let timeOut = null;
+
+    if (totalTime >= 0 && isPlay) {
+      timeOut = setTimeout(() => {
+        setTotalTime(totalTime - 1);
+      }, 1000);
+    }
+
+    changeTimer();
+
+    return () => clearTimeout(timeOut);
+  }, [isPlay, totalTime]);
+
+  const resetAll = () => {
+    const audio = document.getElementById('beep');
+    audio.pause();
+    audio.currentTime = 0;
+    setBreakLength(5);
+    setSessionLength(25);
+    setTotalTime(1500);
+    setIsPlay(false);
+    setTitle('Session');
+  };
+
   return (
     <div className="App">
       <div className="main-container">
@@ -81,9 +100,21 @@ function App() {
             <h2>Break Length</h2>
             <div className="break-buttons">
               <button onClick={incrementBreak} id="break-increment">
+                <FontAwesomeIcon
+                  style={{ height: '100%', width: '100%' }}
+                  onMouseOver={({ target }) => (target.style.color = '#686D76')}
+                  onMouseOut={({ target }) => (target.style.color = 'black')}
+                  icon={faUpLong}
+                />
               </button>
-              <div id="break-length">breakLength</div>
+              <div id="break-length">{breakLength}</div>
               <button onClick={decrementBreak} id="break-decrement">
+                <FontAwesomeIcon
+                  style={{ height: '100%', width: '100%' }}
+                  onMouseOver={({ target }) => (target.style.color = '#686D76')}
+                  onMouseOut={({ target }) => (target.style.color = 'black')}
+                  icon={faDownLong}
+                />
               </button>
             </div>
           </div>
@@ -91,9 +122,21 @@ function App() {
             <h2>Session Length</h2>
             <div className="session-buttons">
               <button onClick={incrementSession} id="session-increment">
+                <FontAwesomeIcon
+                  style={{ height: '100%', width: '100%' }}
+                  onMouseOver={({ target }) => (target.style.color = '#686D76')}
+                  onMouseOut={({ target }) => (target.style.color = 'black')}
+                  icon={faUpLong}
+                />
               </button>
-              <div id="session-length">sessionLength</div>
+              <div id="session-length">{sessionLength}</div>
               <button onClick={decrementSession} id="session-decrement">
+                <FontAwesomeIcon
+                  style={{ height: '100%', width: '100%' }}
+                  onMouseOver={({ target }) => (target.style.color = '#686D76')}
+                  onMouseOut={({ target }) => (target.style.color = 'black')}
+                  icon={faDownLong}
+                />
               </button>
             </div>
           </div>
@@ -101,20 +144,37 @@ function App() {
         <div className="timer-container">
           <div className="timer-wrapper">
             <div id="timer-label">
-              <h2></h2>
+              <h2>{title === 'Session' ? 'Session' : 'Break'}</h2>
             </div>
             <div id="time-left">
-              <strong></strong>
+              <strong>{Timer()}</strong>
             </div>
           </div>
           <div className="timer-control">
-            <button id="start_stop">
+            <button onClick={playTimer} id="start_stop">
+              <FontAwesomeIcon
+                style={{ height: '90%', width: '90%' }}
+                onMouseOver={({ target }) => (target.style.color = '#4B6587')}
+                onMouseOut={({ target }) => (target.style.color = 'black')}
+                icon={isPlay ? faPause : faPlay}
+              />
             </button>
-            <button id="reset">
+            <button onClick={resetAll} id="reset">
+              <FontAwesomeIcon
+                style={{ height: '90%', width: '90%' }}
+                onMouseOver={({ target }) => (target.style.color = '#4B6587')}
+                onMouseOut={({ target }) => (target.style.color = 'black')}
+                icon={faArrowRotateLeft}
+              />
             </button>
           </div>
         </div>
       </div>
+      <audio
+        id="beep"
+        preload="auto"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      ></audio>
     </div>
   );
 }
